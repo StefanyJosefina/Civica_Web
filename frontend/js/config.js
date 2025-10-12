ef35595043a2ac8096a93d61dd36ca507bbc47e6
StefanyJosefina <19623155@std.stei.itb.ac.id>
2025-10-12 14:22:17 +0700

    fix

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://civicaweb-production.up.railway.app";
 
 async function apiFetch(path, options = {}) {
   const token = localStorage.getItem("token");
